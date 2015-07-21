2302a6cc03066633849eb9290cecef95702f4ef1
qingjie <qzhao02@syr.edu>
2015-07-20 21:36:00 -0400

    struct-enum demo

--- struct-enum.playground/section-1.swift
+++ struct-enum.playground/section-1.swift
@@ -2,6 +2,7 @@
 
 import UIKit
 
+//结构体struct和枚举enum的静态属性，静态方法使用static关键字
 struct Account {
     var amount : Double = 0.0
     var owner : String = ""
@@ -13,6 +14,7 @@ struct Account {
     }
 }
 
+//类class的类型属性，类型方法使用class关键字
 class Account1 {
     var amount : Double = 0.0
     var owner : String = ""
